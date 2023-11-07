4142621b43d6a7263a349606d0047f64e5130d3a
黄小莉 <xiaoli.huang@opennews.com.cn>
2023-11-07 14:57:52 +0800

    mod

--- scripts/mysql2oracle.py
+++ scripts/mysql2oracle.py
@@ -33,7 +33,7 @@ database = "wind_db"
 # oracle 连接信息
 lib_dir = "D:\\PLSQL\\product\\11.2.0\\dbhome_1\\bin"
 dsn = "bigdata_admin/qeGAB9Fx5DX32L9D3W5g@192.168.1.91:1521/bigdatadb"
-
+# mysql 字段类型 映射到 oracle 时的字段类型，mysql的date 精确到天,oracle的date 精确到秒
 mysql_to_oracle_map = {
     'int': 'NUMBER',
     'bigint': 'NUMBER',
@@ -50,6 +50,7 @@ mysql_to_oracle_map = {
     'timestamp': 'DATE',
     'date': 'DATE'
 }
+# 获取 mysql 表结构
 sql_query = """SELECT
             C.TABLE_SCHEMA,
             C.TABLE_NAME,
@@ -87,30 +88,33 @@ for result in table_info:
     table_column_comment_infos.append([table_name, column_name, column_comment])
     if table_name not in table_columns:
         table_columns[table_name] = []
-
     column_sql = '\t{} {}'.format(column_name, mysql_to_oracle_map[data_type.decode()])
-
+    # 获取没有精度的数据类型长度 character_octet_length
     if character_octet_length:
         character_octet_length = 4000 if character_octet_length >= 4000 else character_octet_length
         column_sql += '({})'.format(character_octet_length)
+    # 获取有精度的数据类型长度 numeric_precision
     elif numeric_precision:
         column_sql += '({},{})'.format(numeric_precision, numeric_scale)
+    # 判断数据是否为空
     if is_nullable == 'NO':
         column_sql += ' NOT NULL'
     table_columns[table_name].append(column_sql)
-
     # if column_default is not None:
     #     column_sql += ' DEFAULT {}'.format(column_default)
 
+# 创建表, for 只会循环一次
 for key, columns in table_columns.items():
+    # print(table_columns.items())
     create_table_sql += 'CREATE TABLE {} (\n'.format(key)
     create_table_sql += ',\n'.join(columns)
     create_table_sql += '\n);\n\n'
 
     table_comment = table_comment_infos[key]
+    # 表注释
     if table_comment:
         create_table_sql += "COMMENT ON TABLE {} IS '{}';\n\n".format(key, table_comment)
-
+    # 字段注释
     for table_name, column_name, column_comment in table_column_comment_infos:
         if table_name == key and column_comment:
             create_table_sql += "COMMENT ON COLUMN {}.{} IS '{}';\n".format(table_name, column_name, column_comment.decode())
@@ -123,5 +127,4 @@ with open(output_file, 'w') as f1:
     f1.write("{create_table_sql}"
              .format(create_table_sql=create_table_sql.upper()))
 f1.close()
-
 
